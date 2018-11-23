822d76e7881f988e4cc14255f67a7abaa5816566
takahiro.sakoda <sakoda@overwrite.jp>
2018-11-23 18:02:55 +0900

    modaalのギャラリー表示用にrelを追加

--- item/variations/bpsa/index.php
+++ item/variations/bpsa/index.php
@@ -4,7 +4,7 @@
     $num = 1;
     foreach (glob($target_dir . '{*.gif,*.jpg,*.png}', GLOB_BRACE) as $file) {
         if (is_file($file)) {
-            echo '<li><a href="' . htmlspecialchars($file) . '" class="modal-image"><img src="' . htmlspecialchars($file) . '" alt="バリエーション'.$num.'"></a></li>';
+            echo '<li><a href="' . htmlspecialchars($file) . '" class="modal-image" rel="gallery"><img src="' . htmlspecialchars($file) . '" alt="バリエーション'.$num.'"></a></li>';
         }
     $num++;
     }
