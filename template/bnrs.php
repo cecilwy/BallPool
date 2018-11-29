ea38cdb2b5da76a6b42f4764bded208cf303661a
K-Hamai <33743746+K-Hamai@users.noreply.github.com>
2018-11-29 21:49:08 +0900

    スライダーバナー　修正

--- template/bnrs.php
+++ template/bnrs.php
@@ -1,11 +1,15 @@
 <div class="bnrs">
     <ul class="list-slider">
-        <li><a href="#"><img src="/img/bnr_01.png" alt="ミニボールプール"></a></li>
-        <li><a href="#"><img src="/img/bnr_02.png" alt="ボールプール（中型）"></a></li>
-        <li><a href="#"><img src="/img/bnr_03.png" alt="ボールプール（大型）"></a></li>
-        <li><a href="#"><img src="/img/bnr_04.png" alt="ボールプール関連商品"></a></li>
-        <li><a href="#"><img src="/img/bnr_05.png" alt="ボールプール関連商品"></a></li>
-        <li><a href="#"><img src="/img/bnr_06.png" alt="ボールプール関連商品"></a></li>
+        <li><a href="#"><img src="/img/bnr_01.png" alt="円型ボールプール"></a></li>
+        <li><a href="#"><img src="/img/bnr_02.png" alt="ミニボールプール"></a></li>
+        <li><a href="#"><img src="/img/bnr_03.png" alt="ボールプールCセット"></a></li>
+        <li><a href="#"><img src="/img/bnr_04.png" alt="滑り台・クライミングセット"></a></li>
+        <li><a href="#"><img src="/img/bnr_05.png" alt="２段ミニボールプールセット"></a></li>
+        <li><a href="#"><img src="/img/bnr_06.png" alt="ボールプールAセット"></a></li>
+        <li><a href="#"><img src="/img/bnr_07.png" alt="滑り台２個・クライミングセット"></a></li>
+        <li><a href="#"><img src="/img/bnr_08.png" alt="ボールプールBセット"></a></li>
+        <li><a href="#"><img src="/img/bnr_09.png" alt="滑り台２個セット"></a></li>
+        <li><a href="#"><img src="/img/bnr_10.png" alt="ボールプールカメさんセット"></a></li>
     </ul>
     <p class="btn01"><a href="#">ご相談・お見積もり</a></p>
 </div>
