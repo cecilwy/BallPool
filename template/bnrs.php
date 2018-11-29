d3f2fc612b88eb7a425b540b9527b854968d4889
K-Hamai <33743746+K-Hamai@users.noreply.github.com>
2018-11-29 21:55:17 +0900

    スライダーバナー　修正

--- template/bnrs.php
+++ template/bnrs.php
@@ -6,10 +6,6 @@
         <li><a href="#"><img src="/img/bnr_04.png" alt="滑り台・クライミングセット"></a></li>
         <li><a href="#"><img src="/img/bnr_05.png" alt="２段ミニボールプールセット"></a></li>
         <li><a href="#"><img src="/img/bnr_06.png" alt="ボールプールAセット"></a></li>
-        <li><a href="#"><img src="/img/bnr_07.png" alt="滑り台２個・クライミングセット"></a></li>
-        <li><a href="#"><img src="/img/bnr_08.png" alt="ボールプールBセット"></a></li>
-        <li><a href="#"><img src="/img/bnr_09.png" alt="滑り台２個セット"></a></li>
-        <li><a href="#"><img src="/img/bnr_10.png" alt="ボールプールカメさんセット"></a></li>
     </ul>
     <p class="btn01"><a href="#">ご相談・お見積もり</a></p>
 </div>
