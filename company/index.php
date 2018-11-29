b93eb3257fde6dd7dcfed6ab6a84a67717589b9c
takahiro.sakoda <sakoda@overwrite.jp>
2018-11-29 15:24:23 +0900

    会社案内に沿革のテキストを追加

--- company/index.php
+++ company/index.php
@@ -86,7 +86,7 @@ $description = '';
                             <th>関連会社</th>
                             <td>有限会社プロダクトワークス<br/>
                                 店舗家具株式会社<br/>
-                                和泉フォーム株式会社(<a href="#" target="_blank">ホームページ</a>)
+                                和泉フォーム株式会社(<a href="//www.izumi-foam.co.jp/" target="_blank">ホームページ</a>)
                             </td>
                         </tr>
 
@@ -96,59 +96,180 @@ $description = '';
                 <div class="box">
                     <h2 class="head01"><span>アクセス</span></h2>
                     <div class="map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6562.309024737287!2d135.61143276889447!3d34.676049618198796!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf7d4fd0b81a8dc54!2z5qCq5byP5Lya56S-44Ov44O844Kv44K5!5e0!3m2!1sja!2sus!4v1413531749205" frameborder="0" style="border:0"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6562.309024737287!2d135.61143276889447!3d34.676049618198796!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf7d4fd0b81a8dc54!2z5qCq5byP5Lya56S-44Ov44O844Kv44K5!5e0!3m2!1sja!2sus!4v1413531749205"
+                                frameborder="0" style="border:0"></iframe>
                     </div>
                 </div>
-
 
 
                 <div class="box">
                     <h2 class="head01"><span>沿革</span></h2>
 
-                    <table>
+                    <table id="enkaku">
                         <tr>
                             <th>平成10年1月</th>
                             <td>株式会社ワークス設立し創業開始 主に特注、別注家具の製作、籐製品の輸入卸販売を行う</td>
                         </tr>
                         <tr>
                             <th>平成11年4月</th>
-                            <td></td>
+                            <td>【WISM】業務用椅子カタログを発刊</td>
                         </tr>
                         <tr>
                             <th>平成12年5月</th>
-                            <td></td>
+                            <td>【WISM】ホームページを開設<br/>
+                                <a href="//www.wism-chair.com" target="_blank">www.wism-chair.com</a>
+
+                            </td>
                         </tr>
                         <tr>
                             <th>平成15年5月</th>
-                            <td></td>
+                            <td>東京営業所を開設</td>
                         </tr>
                         <tr>
                             <th>平成16年5月</th>
-                            <td></td>
-                        </tr>
-
-
-                        <tr>
-                            <th>平成16年5月</th>
-                            <td></td>
+                            <td>【椅子の張替えドットコム】ホームページを開設<br/>
+                                <a href="//www.isunoharikae.com" target="_blank">www.isunoharikae.com</a></td>
                         </tr>
 
 
                         <tr>
                             <th>平成16年5月</th>
-                            <td></td>
+                            <td>【張替え太郎】ホームページを開設<br/>
+                                <a href="//www.harikaetaro.com" target="_blank">www.harikaetaro.com</td>
                         </tr>
 
 
                         <tr>
-                            <th>平成16年5月</th>
-                            <td></td>
+                            <th>平成16年7月</th>
+                            <td>業務用家具メーカーのOEM生産開始</td>
                         </tr>
 
 
                         <tr>
-                            <th>平成16年5月</th>
-                            <td></td>
+                            <th>平成17年3月</th>
+                            <td>【ベンチシートドットコム】ホームページを開設<br/>
+                                <a href="//www.benchsheet.com" target="_blank">www.benchsheet.com</a></td>
+                        </tr>
+
+
+                        <tr>
+                            <th>平成17年8月</th>
+                            <td>【ベンチシートどんと来いドットコム】ホームページを開設<br/>
+                                <a href="//www.benchsheetdontkoi.com" target="_blank">www.benchsheetdontkoi.com</a></td>
+                        </tr>
+
+                        <tr>
+                            <th>平成18年1月</th>
+                            <td>自社工場を分社化し、有限会社プロダクトワークスを設立</td>
+                        </tr>
+
+                        <tr>
+                            <th>平成19年6月</th>
+                            <td>自大阪府経営革新計画承認企業の認定を受ける</td>
+                        </tr>
+
+                        <tr>
+                            <th>平成20年3月</th>
+                            <td>【ベンチチェアードットコム】ホームページを開設<br/>
+                                <a href="//www.bench-chair.com" target="_blank">www.bench-chair.com</a></td>
+                        </tr>
+
+
+                        <tr>
+                            <th>平成20年7月</th>
+                            <td>【椅子の張り地ドットコム】ホームページを開設<br/>
+                                <a href="//www.isunohariji.com" target="_blank">www.isunohariji.com</a></td>
+                        </tr>
+
+
+                        <tr>
+                            <th>平成20年10月</th>
+                            <td>社内に撮影スタジオ設置</td>
+                        </tr>
+
+
+                        <tr>
+                            <th>平成21年10月</th>
+                            <td>【テーブル店舗ドットコム】ホームページを開設<br/>
+                                <a href="//www.tabletenpo.com" target="_blank">www.tabletenpo.com</a></td>
+                        </tr>
+
+
+                        <tr>
+                            <th>平成21年6月</th>
+                            <td>【ホームページの制作部門を分社化し、店舗家具株式会社を設立</td>
+                        </tr>
+
+                        <tr>
+                            <th>平成21年10月</th>
+                            <td>【キッズコーナー遊び場】ホームページを開設<br/>
+                                <a href="//www.kidscornerasobiba.com" target="_blank">www.kidscornerasobiba.com</a></td>
+                        </tr>
+
+                        <tr>
+                            <th>平成23年2月</th>
+                            <td>防炎製品の認定工場として承認</td>
+                        </tr>
+
+
+                        <tr>
+                            <th>平成23年4月</th>
+                            <td>楽天市場に出店</td>
+                        </tr>
+
+                        <tr>
+                            <th>平成23年6月</th>
+                            <td>ヤフーショッピングに出店</td>
+                        </tr>
+
+                        <tr>
+                            <th>平成23年7月</th>
+                            <td>【キッズプレイグラウンド】ホームページを開設<br/>
+                                <a href="//www.kodomoplayground.com" target="_blank">www.kodomoplayground.com</a></td>
+                        </tr>
+
+                        <tr>
+                            <th>平成24年2月</th>
+                            <td>【チャイルドコーナー】ホームページを開設<br/>
+                                <a href="//www.child-corner.com" target="_blank">www.child-corner.com</a></td>
+                        </tr>
+
+
+                        <tr>
+                            <th>平成25年9月</th>
+                            <td>アマゾンに出店</td>
+                        </tr>
+
+                        <tr>
+                            <th>平成26年10月</th>
+                            <td>ボールプール用ボール輸入開始</td>
+                        </tr>
+
+
+                        <tr>
+                            <th>平成27年10月</th>
+                            <td>【テーブルナビドットコム】ホームページを開設<br/>
+                                <a href="//www.table-navi.com" target="_blank">www.table-navi.com</a></td>
+                        </tr>
+
+
+                        <tr>
+                            <th>平成28年4月</th>
+                            <td>【店舗家具ショップドットコム】ホームページを開設<br/>
+                                <a href="//www.tempokagushop.com" target="_blank">www.tempokagushop.com</a></td>
+                        </tr>
+
+
+                        <tr>
+                            <th>平成28年5月</th>
+                            <td>【ベンチソファドットコム】ホームページを開設<br/>
+                                <a href="//www.bench-sofa.com" target="_blank">www.bench-sofa.com</a></td>
+                        </tr>
+
+                        <tr>
+                            <th>平成29年4月</th>
+                            <td> 【COTOKITO】子ども用木製家具ブランド「コトキト」設立<br/>
+                                <a href="//www.coto-kito.com" target="_blank">www.coto-kito.com</a></td>
                         </tr>
 
                     </table>
