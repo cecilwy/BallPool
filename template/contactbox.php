4d30319d994aea4305c5b99e5b64bb31948f3a7f
takahiro.sakoda <sakoda@overwrite.jp>
2018-12-05 14:12:07 +0900

    カラー機能のアイコンを設置

---
+++ template/contactbox.php
@@ -0,0 +1,20 @@
+<div class="box">
+    <h2 class="head01 adjust">電話・メールでのお問い合わせ</h2>
+    <p>ボールプールドットコムのご購入商品およびご利用方法につきましては下記、電話またはメールよりお問い合わせ下さい。<br />
+        尚、等お問い合わせ窓口はボールプールドットコムに関する専用窓口です。</p>
+
+    <div class="contact-btn-wrapper">
+        <div class="contact-btn">
+            <div class="contact-btn-inner">
+                <a href="tel:0120-71-1010"><img src="/contact/img/btn_tel.png" alt="電話（フリーダイヤル）でのお問い合わせ 0120-71-1010" /></a>
+                <p>※受付時間(月～日):09:00～18:00 夏期休業、年末年始はお休みさせていただきます。</p>
+            </div>
+        </div>
+        <div class="contact-btn">
+            <div class="contact-btn-inner">
+                <a href="#"><img src="/contact/img/btn_email.png" alt="メールでのお問い合わせ" /></a>
+                <p>※お問い合わせいただきました順にご対応をさせていただきますが、対応までお時間をいただく場合もございます。</p>
+            </div>
+        </div>
+    </div>
+</div>
