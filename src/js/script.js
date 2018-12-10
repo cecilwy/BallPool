d70b45fa90d63a13c21e9869fd51ba60456fdf98
takahiro.sakoda <sakoda@overwrite.jp>
2018-12-10 18:16:51 +0900

    #15 smooth scroll の調整

--- src/js/script.js
+++ src/js/script.js
@@ -49,8 +49,8 @@ jQuery(function () {
 
     jQuery('a[href^="#"]').click(function(){
         var speed = 500;
-        var href= $(this).attr("href");
-        var target = $(href == "#" || href == "" ? 'html' : href);
+        var href= jQuery(this).attr("href");
+        var target = jQuery(href == "#" || href == "" ? 'html' : href);
         var position = target.offset().top;
         jQuery("html, body").animate({scrollTop:position}, speed, "swing");
         return false;
